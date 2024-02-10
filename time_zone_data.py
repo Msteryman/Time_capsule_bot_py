bfb2e6ee8efc73dce3e4c2025de9b21e693f93fc
Msteryman <123405107+Msteryman@users.noreply.github.com>
2024-02-11 01:22:29 +0400

    add time zone

--- time_zone_data.py
+++ time_zone_data.py
@@ -1,15 +1,14 @@
-import sqlite3
+import pandas as pd
+dic
+# Указываем путь к файлу Excel
+excel_file = 'asddas.xlsx'
 
-def create_table_todo(): # Создание таблицы при запуске скрипта
-    
-    conn = sqlite3.connect('time_date.db')
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS time_date( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        User_id INTEGER,
-        msg text,   
-        date text
-    )""")
-    conn.commit()
-    conn.close()
-create_table_todo()
+# Чтение данных из Excel-файла в DataFrame
+df = pd.read_excel(excel_file)
+first_column = df.iloc[:, 0]
+# Выводим первые несколько строк DataFrame для проверки
+print(first_column.head())
+first_column_dict = first_column.to_dict()
+
+for i in first_column_dict.items():
+    print(i[1])
