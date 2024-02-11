3b3f60215bb08cc598719cafc5a70e174cd1db63
Msteryman <123405107+Msteryman@users.noreply.github.com>
2024-02-11 22:35:13 +0400

    add dist with time_zone

--- time_zone_data.py
+++ time_zone_data.py
@@ -1,14 +1,32 @@
 import pandas as pd
-dic
+
 # Указываем путь к файлу Excel
 excel_file = 'asddas.xlsx'
 
 # Чтение данных из Excel-файла в DataFrame
 df = pd.read_excel(excel_file)
-first_column = df.iloc[:, 0]
+first_column = df.iloc[:, 2]
 # Выводим первые несколько строк DataFrame для проверки
-print(first_column.head())
+
+first_column_dict = first_column.to_dict()
+dict_1 = []
+for i in first_column_dict.items():
+    dict_1.append(i[1].replace('\xa0',''))
+
+dict_2 = []
+df = pd.read_excel(excel_file)
+first_column = df.iloc[:, 0]
 first_column_dict = first_column.to_dict()
 
-for i in first_column_dict.items():
-    print(i[1])
+for j,i in enumerate(first_column_dict.items()):
+    i = list(i)
+    if  type(i[1]) == float:
+        i[1] = "null"
+        dict_2.append(i[1])
+    else:
+        dict_2.append(i[1].replace('\xa0',''))
+        
+
+
+dict_final = dict(zip(dict_1,dict_2))
+print(dict_final)
